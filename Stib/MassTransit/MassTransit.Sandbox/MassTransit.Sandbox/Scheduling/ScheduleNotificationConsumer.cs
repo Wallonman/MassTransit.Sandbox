7c3b4727e7968ffe6de2eef7e1447aa9ec796ddb
epiraux <epiraux@26d859b9-3f35-0947-bafd-1245edf3a170>
2018-03-23 16:03:24 +0000

    Scheduling (tbc)

---
+++ Stib/MassTransit/MassTransit.Sandbox/MassTransit.Sandbox/Scheduling/ScheduleNotificationConsumer.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Threading.Tasks;
+
+namespace MassTransit.Sandbox.Scheduling
+{
+    internal class ScheduleNotificationConsumer : IConsumer<IScheduleNotification>
+    {
+
+        public async Task Consume(ConsumeContext<IScheduleNotification> context)
+        {
+            await Console.Out.WriteLineAsync($"Received a IScheduleNotification to {context.Message.EmailAddress}");
+            await Console.Out.WriteLineAsync($"Sending at {DateTime.Now} a SendNotification to {context.Message.EmailAddress} for {context.Message.DeliveryTime}");
+
+            await context.ScheduleSend(new Uri("rabbitmq://localhost/notification_queue"),
+                context.Message.DeliveryTime,
+                new SendNotificationCommand
+                {
+                    EmailAddress = context.Message.EmailAddress,
+                    Body = context.Message.Body
+                });
+        }
+
+        public class SendNotificationCommand :
+            ISendNotification
+        {
+            public string EmailAddress { get; set; }
+            public string Body { get; set; }
+        }
+    }
+}
