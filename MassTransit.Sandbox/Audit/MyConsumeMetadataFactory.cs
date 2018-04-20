1fad0ca372613369a7cab46e9bf670847bf9fc02
Piraux Eric <Eric.Piraux@stib.brussels>
2018-04-20 08:27:26 +0200

    adding metadatfactory

--- MassTransit.Sandbox/Audit/MyConsumeMetadataFactory.cs
+++ MassTransit.Sandbox/Audit/MyConsumeMetadataFactory.cs
@@ -1,34 +1,22 @@
-﻿using System;
 using System.Linq;
-using System.Threading.Tasks;
 using MassTransit.Audit;
-using Newtonsoft.Json;
 
 namespace MassTransit.Sandbox.Audit
 {
-    public class AuditStore : IMessageAuditStore
-    {
-        private readonly string _code;
-
-        public AuditStore(string code)
-        {
-            _code = code;
-        }
-
-        public async Task StoreMessage<T>(T message, MessageAuditMetadata metadata) where T : class
-        {
-                await Console.Out.WriteLineAsync($"{_code} Payload: {JsonConvert.SerializeObject(message)}");
-                await Console.Out.WriteLineAsync($"{_code} Metadata: {JsonConvert.SerializeObject(metadata)}");
-        }
-    }
-
     public class MyConsumeMetadataFactory : IConsumeMetadataFactory
     {
+        private readonly string _contextType;
+
+        public MyConsumeMetadataFactory(string contextType)
+        {
+            _contextType = contextType;
+        }
+
         public MessageAuditMetadata CreateAuditMetadata<T>(ConsumeContext<T> context) where T : class
         {
             return new MessageAuditMetadata
             {
-                ContextType = "contextType",
+                ContextType = _contextType,
                 ConversationId = context.ConversationId,
                 CorrelationId = context.CorrelationId,
                 InitiatorId = context.InitiatorId,
